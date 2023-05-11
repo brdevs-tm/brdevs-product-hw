6b51fd8b3a54d733adfb46c43a5247b56a403774
khamidov <bright19.off@gmail.com>
2023-05-11 14:53:12 +0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,16 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:47,
+    nav:false,
+    responsive:{
+        0:{
+            items: 1
+        },
+        600:{
+            items:1
+        },
+        1160:{
+            items:3
+        }
+    }
+})
